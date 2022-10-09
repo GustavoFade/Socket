8844923caa5ac9dd183771d34152a8c47f053bd3
Gustavo Fadel <Gustavo-959694@hotmail.com>
2022-10-09 10:31:20 -0300

    adicionado os metódos do socket.io para estudo

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const path = require('path');
 const socketIO = require('socket.io');
+const PORT = 3000;
 
 const app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+// app.get('/', express.json(), (req,res)=> res.send("dslkdslkds"));
+const server = app.listen(PORT, () => {
+    console.log(`Running at port: ${PORT}`);
+})
+
+const io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    // socket.emit("hi", {msg: "Mensagem enviada do back"}); //manda mensagem para o front
+    // socket.on('hello',(data)=>{ //recebe mensagem do front
+    //     console.log(data.msg)})
+
+    // io.emit('hi', { msg: "enviando para todo mundo/ok" });//envia para todo mundo
+
+    socket.broadcast.emit('hi', { msg: "envia para todo mundo menos para o socket da conexão"})
+    
+})
